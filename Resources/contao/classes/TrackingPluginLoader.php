c7c8fefab397b88cd38c09695c01619672528c14
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-10-20 15:52:16 +0200

    fix web pathes in TrackingPluginLoader

--- Resources/contao/classes/TrackingPluginLoader.php
+++ Resources/contao/classes/TrackingPluginLoader.php
@@ -25,21 +25,21 @@ class TrackingPluginLoader
 
         // load language script
         if ($GLOBALS['TL_LANGUAGE'] == 'de') {
-            $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilterconstant-i18n'] = 'system/modules/con4gis_tracking/assets/js/c4g-maps-plugin-trackingfilter-constant-i18n-de.js';
+            $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilterconstant-i18n'] = 'bundles/con4gistracking/js/c4g-maps-plugin-trackingfilter-constant-i18n-de.js';
         } else {
             // use english as fallback
-            $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilterconstant-i18n'] = 'system/modules/con4gis_tracking/assets/js/c4g-maps-plugin-trackingfilter-constant-i18n-en.js';
+            $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilterconstant-i18n'] = 'bundles/con4gistracking/js/c4g-maps-plugin-trackingfilter-constant-i18n-en.js';
         }
 
 
-        $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilter-constant'] = 'system/modules/con4gis_tracking/assets/js/c4g-maps-plugin-trackingfilter-constant.js';
+        $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilter-constant'] = 'sbundles/con4gistracking/js/c4g-maps-plugin-trackingfilter-constant.js';
 
-        $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilter-jquery-simple-datetimepicker'] = 'system/modules/con4gis_tracking/assets/js/jquery-simple-datetimepicker/1.13.0/jquery.simple-dtpicker.js';
+        $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilter-jquery-simple-datetimepicker'] = 'bundles/con4gistracking/js/jquery-simple-datetimepicker/1.13.0/jquery.simple-dtpicker.js';
 
-        $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilter'] = 'system/modules/con4gis_tracking/assets/js/c4g-maps-plugin-trackingdatafilter.js';
+        $GLOBALS['TL_JAVASCRIPT']['c4g-maps-plugin-trackingdatafilter'] = 'bundles/con4gistracking/js/c4g-maps-plugin-trackingdatafilter.js';
 
-        $GLOBALS['TL_CSS']['c4g-maps-plugin-trackingdatafilter-jquery-simple-datetimepicker'] = 'system/modules/con4gis_tracking/assets/js/jquery-simple-datetimepicker/1.13.0/jquery.simple-dtpicker.css';
-        $GLOBALS['TL_CSS']['c4g-maps-plugin-trackingdatafilter'] = 'system/modules/con4gis_tracking/assets/css/c4g-maps-plugin-trackingdatafilter.css';
+        $GLOBALS['TL_CSS']['c4g-maps-plugin-trackingdatafilter-jquery-simple-datetimepicker'] = 'bundles/con4gistracking/js/jquery-simple-datetimepicker/1.13.0/jquery.simple-dtpicker.css';
+        $GLOBALS['TL_CSS']['c4g-maps-plugin-trackingdatafilter'] = 'bundles/con4gistracking/css/c4g-maps-plugin-trackingdatafilter.css';
 
     }
 }
