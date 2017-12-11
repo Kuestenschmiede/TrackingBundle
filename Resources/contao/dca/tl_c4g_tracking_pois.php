ffa83e9d7adca70d7dd56c4c1064d427187d723b
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-12-11 07:53:15 +0100

    wrong namespace for C4GAutomator

--- Resources/contao/dca/tl_c4g_tracking_pois.php
+++ Resources/contao/dca/tl_c4g_tracking_pois.php
@@ -30,7 +30,7 @@ $GLOBALS['TL_DCA']['tl_c4g_tracking_pois'] = array
 			//array('tl_module', 'checkPermission')
 		),
         'onsubmit_callback'           => array(
-            array('\c4g\Core\C4GAutomator', 'purgeApiCache')
+            array('\con4gis\CoreBundle\Resources\contao\classes\C4GAutomator', 'purgeApiCache')
         ),
 		'sql' => array
 		(
